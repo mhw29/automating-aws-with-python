3a360767f03da152e236a0202bb6f62f633dfc0d
Matt Williamson <mwilliamson@cellularoptimization.com>
2020-03-14 08:12:07 -0500

    .

--- webotron/webotron.py
+++ webotron/webotron.py
@@ -1,7 +1,14 @@
 import boto3
+import click
+
 session = boto3.Session(profile_name="default")
 s3 = session.resource('s3')
 
-if __name__ == '__main__':
+@click.command('list_buckets')
+def list_buckets():
+  #List all buckets
     for bucket in s3.buckets.all():
-      print(bucket)
+     print(bucket)
+     
+if __name__ == '__main__':
+    list_buckets()
